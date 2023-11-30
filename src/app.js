55a22ecfceed4874f2af0a5f993264e1a0dc8b2f
Mitul Vaiyata <78469796+mitul3011@users.noreply.github.com>
2023-11-30 17:02:17 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+
+const express = require('express');
+const aws = require('aws-sdk');
+const config = require('./config.js');
+const crypto = require('crypto');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+aws.config.update(config.aws_remote_config);
+let dynamoDB = new aws.DynamoDB();
+
+let createShortURL = (id) => {
+    return  Date.now() % 100 + crypto.randomUUID().substring(32, 36);
+};
+
+app.post('/createNewShortUrl', async (req, res) => {
+    const fullUrl = req.body.fullUrl;
+    const expiresAt = Date.now() + req.body.expires * 24 * 60 * 60 * 1000;
+
+    const clicks = "0";
+
+    // return id to the user
+    const id = createShortURL();
+
+    let params = {
+        TableName: config.aws_table_name,
+        Item: {
+            id: {S: id},
+            fullurl: {S: fullUrl},
+            clicks: {N: clicks},
+            expiresAt: {N: expiresAt.toString()}
+        }
+    };
+
+    dynamoDB.putItem(params, (err, data) => {
+        if(err)
+            console.log("Error", err);
+        else
+            console.log("Success!");
+    });
+
+    res.send({ id });
+});
+
+app.get('/:shortUrl', (req, res) => {
+    let params = {
+        TableName: config.aws_table_name,
+        Key: {
+            'id': {S: req.params.shortUrl}
+        }
+    };
+
+    dynamoDB.getItem(params, (err, data) => {
+        if(err)
+            console.log(err.message);
+
+        res.send( data.Item.fullurl );
+    });
+});
+
+app.listen(port, () => {
+    console.log('Server is up on port', port);
+});
